3e9637f15a6ac2dcbb517dbedcfaee0ec437f444
Alex <sanchopa@mail.ru>
2016-11-26 01:24:16 +0600

    Create UI

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Launcher {
     public static void main(String[] args) {
+        JFrame myWindow = new Dialog();
+        myWindow.setVisible(true);
 
     }
 }
